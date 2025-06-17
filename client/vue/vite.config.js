1c929bc25d5a21981f2d9b6a9dbd01c1916c32c6
Filipe Lago Peres <filipelperes@gmail.com>
2025-06-17 18:28:18 -0300

    add build

---
+++ client/vue/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => ({
+    plugins: [vue(), tailwindcss()],
+    base: mode !== 'development' ? '/vue/' : '',
+    server: {
+        port: 5172,
+        host: true,
+        proxy: {
+            '^/api/.*': 'http://localhost:3333'
+        }
+    },
+    build: {
+        outDir: '../../dist/vue',
+        emptyOutDir: true
+    },
+    resolve: {
+        alias: {
+            // '@': path.resolve(__dirname, '../../server/src'),
+            '@/vue': path.resolve(__dirname, './src')
+        }
+    }
+}));
